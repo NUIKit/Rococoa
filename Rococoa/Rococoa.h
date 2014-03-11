39068a594bcb7e2e3a9e7ca1927b22d5b4f7c2f2
CodaFi <devteam.codafi@gmail.com>
2014-03-11 17:22:16 -0600

    Initial Commit

---
+++ Rococoa/Rococoa.h
@@ -0,0 +1,12 @@
+//
+//  Rococoa.h
+//  Rococoa
+//
+//  Created by Robert Widmann on 3/11/14.
+//  Copyright (c) 2014 Robert Widmann. All rights reserved.
+//  Released under the MIT license.
+//
+
+#import <Foundation/Foundation.h>
+#import <NUIKit/NUIKit.h>
+#import <CoreData/CoreData.h>
